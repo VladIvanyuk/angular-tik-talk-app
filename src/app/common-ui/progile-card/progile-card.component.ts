9d411015950fe81ede8c0cd59d3c46f3470289b5
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-07-22 20:46:01 +1000

    [+] init app

---
+++ src/app/common-ui/progile-card/progile-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-progile-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './progile-card.component.html',
+  styleUrl: './progile-card.component.scss'
+})
+export class ProgileCardComponent {
+
+}
