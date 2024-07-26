1606b1957b94b5f7f34c45f26131f1a4b194df62
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-07-26 13:26:52 +1000

    [+] add login and main routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
-import { ProfileService } from './data/services/profile.service';
-import { IProfile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +9,4 @@ import { IProfile } from './data/interfaces/profile.interface';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  profileSevice = inject(ProfileService);
-  profiles: IProfile[] = [];
-
-  constructor() {
-    this.profileSevice.getTestAccounts().subscribe((data) => {
-      this.profiles = data;
-    });
-  }
-}
+export class AppComponent {}
---
+++ src/app/pages/search-page/search-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { ProfileCardComponent } from '../../common-ui/profile-card/profile-card.component';
+import { ProfileService } from '../../data/services/profile.service';
+import { IProfile } from '../../data/interfaces/profile.interface';
+
+@Component({
+  selector: 'app-search-page',
+  standalone: true,
+  imports: [ProfileCardComponent],
+  templateUrl: './search-page.component.html',
+  styleUrl: './search-page.component.scss',
+})
+export class SearchPageComponent {
+  profileSevice = inject(ProfileService);
+  profiles: IProfile[] = [];
+
+  constructor() {
+    this.profileSevice.getTestAccounts().subscribe((data) => {
+      this.profiles = data;
+    });
+  }
+}
