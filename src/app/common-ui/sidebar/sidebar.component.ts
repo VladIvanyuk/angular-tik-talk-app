cfd3f95ea7dd5060ee24dd02fbc3220d1f4385b9
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-07-31 12:29:24 +1000

    [+] sidebar

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent {}
---
+++ src/app/common-ui/user-preview/user-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-preview.component.html',
+  styleUrl: './user-preview.component.scss'
+})
+export class UserPreviewComponent {
+
+}
